2918e09efbfe9b43612baa40f7d5bac77de28c74
dsantano <danielsantanof99@gmail.com>
2019-05-17 01:31:56 +0200

    Primeros pasos del FormBean y Img en productos

--- AzerothsPcs_V1/src/main/java/com/salesianostriana/dam/configuracion/MvcConfig.java
+++ AzerothsPcs_V1/src/main/java/com/salesianostriana/dam/configuracion/MvcConfig.java
@@ -1,16 +1,27 @@
 package com.salesianostriana.dam.configuracion;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/login");
 		registry.addViewController("/AccesoDenegado");
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry
+			.addResourceHandler("/img/**")
+			.addResourceLocations("file:files/");	
+			
 	}
 
 	
---
+++ AzerothsPcs_V1/src/main/java/com/salesianostriana/dam/controller/ProductoFormBean.java
@@ -0,0 +1,11 @@
+package com.salesianostriana.dam.controller;
+
+import org.springframework.stereotype.Controller;
+
+import com.salesianostriana.dam.service.ProductoService;
+
+@Controller
+public class ProductoFormBean {
+
+	ProductoService productoService;
+}
